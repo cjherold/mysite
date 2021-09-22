461826b9eeccd643e98beba557056da086cf0068
Chris <64932887+cjherold@users.noreply.github.com>
2021-09-21 20:11:36 -0500

    first

---
+++ docs/js/home.js
@@ -0,0 +1,39 @@
+
+const skills = [
+    {
+        img: '../img/',
+        name: 'Node'
+    },
+    {
+        img: '../img/',
+        name: 'SQL'
+    },
+    {
+        img: '../img/',
+        name: 'Sass'
+    },
+    {
+        img: '../img/',
+        name: 'HTML'
+    },
+    {
+        img: '../img/',
+        name: 'Node'
+    },
+    {
+        img: '../img/',
+        name: 'Node'
+    },
+    {
+        img: '../img/',
+        name: 'Node'
+    },
+    {
+        img: '../img/',
+        name: 'Node'
+    },
+    {
+        img: '../img/',
+        name: 'Node'
+    },
+]
---
+++ docs/js/navbar.js
@@ -0,0 +1,21 @@
+// Loads navbar from here. All changes here affect all navbars.
+const content = () => {
+    return `
+    <a class="navbar-brand cust-font link-text" href="./index.html">Home</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item cust-font">
+                <a class="nav-link link-text" href="./soon.html">Sudoku</a>
+            </li>
+            <!-- <li class="nav-item cust-font">
+                <a class="nav-link link-text" href="./soon.html">Soon</a>
+            </li> -->
+        </ul>
+    </div>
+`}
+
+$('#navbar').append(content);
+
