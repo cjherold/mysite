bc2e3b7c5bf06bd258b52953ad183f0dc8f52bf7
Chris <64932887+cjherold@users.noreply.github.com>
2022-01-16 23:18:42 -0600

    cleaning up puzzle code

--- docs/js/Game.js
+++ docs/js/Game.js
@@ -1,5 +1,4 @@
 
-
 /**
  * Main game class.
  * Selects loadout, shuffles, checks for win, new game
@@ -8,202 +7,57 @@
  class Game {
     constructor(difficulty) {
         const difficultyNumber = {
-            'easy': 60,
-            'medium': 50,
-            'hard': 35
-        }
+            easy: 60,
+            medium: 50,
+            hard: 35,
+        };
         this.difficulty = difficultyNumber[difficulty];
         const selected = Math.floor(Math.random() * loadouts.length);
         this.loadout = loadouts[selected] || [];
     }
 
-    // ----------------------------
-    // Build empty puzzle board on first page load.
-    // This is a template of how the p tags are going to be structured
-    // ----------------------------
-    createBoard() {
-        const puzzleRow = `
-                <div class="puzzle-tile d-flex">
-                    <p 
-                        class="m-auto tile-number covered"
-                        data-correctanswer=""
-                        data-correct="0"
-                        data-changeable="no"
-                    ></p>
-                </div>
-            `.repeat(9);
-
-        const board = `
-                <div class="row d-flex justify-content-center puzzle-row">
-                    ${puzzleRow}
-                </div>
-            `.repeat(9);
-        return board;
-    }
-
     /**
-     *  Setup on page load
+     *  Builds the html for the puzzle container
      */
     initialize() {
-        this.swapLoadoutValues(2);
-
-        this.shuffleSections();
-
+        // Prepare the loadout
+        this.swapLoadoutValues({ numberOfSwaps: 2 });
+        this.shuffleLoadout();
         this.flattenLoadout();
 
-
-        this.fillBoard();
-
-        // Picker wheel buttons functionality
-        this.pickerWheelFunctionality();
-
-        // Tiles spawn picker wheel on click
-        this.tileClickFunctionality();
-
-
+        // Using the loadout, apply values to each tile on the board
+        return this.createBoard();
     }
-
-
+ 
     /**
-     *  Tile click functionality
+     * Picks 2 random numbers and swaps them in the loadout
      */
-    tileClickFunctionality() {
-        // Tile on click, spawns a picker wheel
-        $('.puzzle-tile').on('click', function (e) {
-            const x = e.pageX;
-            const y = e.pageY;
-            const currentTile = e.target.firstElementChild;
-            const changeable = e.target.firstElementChild.getAttribute('data-changeable');
-            // console.log('changeable', changeable);
-            if (changeable === 'yes') {
-                // console.log('tripped');
-                spawnPickerWheel(x, y, currentTile);
-            }
-            
-        });
-
-        // Show wheel
-        const spawnPickerWheel = (x, y, currentTile) => {
-            const correctanswer = currentTile.attributes[2].value || 'nope';
-            const id = currentTile.id;
-            // const isHidden = e.target.firstElementChild.getAttribute('data-isHidden');
-            // console.log('correct answer: ' + correctanswer);
-            // console.log('id??: ', id);
-            $('#picker-wheel').attr('data-tilenumber', id);
-            // console.log(`x: ${x}, y: ${y}`);
-            x = x - 130;
-            y = y - 130;
-            // console.log('id: ' + tileNumber);
-            if (screen.width > 700) {
-                $('#picker-wheel').css({
-                    "top": y,
-                    "left": x
-                });
-            } else {
-                const xOffset = (screen.width / 2 - 125);
-                const yOffset = (screen.height / 2 - 125);
-                $('#picker-wheel').css({
-                    "bottom": `${yOffset}px`,
-                    "left": `${xOffset}px`
-                });
-            }
-        
-            $('#picker-wheel').removeClass('d-none').show(800);
-
-            // Check if tile correct or not
-        }
-    }
-
-
-    checkAnswer(chosen, correct, tile) {
-        if (chosen === correct) {
-            tile.currentTile.attributes[2].value = 1;
-            tile.removeClass('covered');
-            // console.log('correct');
-        } else {
-            tile.currentTile.attributes[2].value = 0;
-            tile.addClass('covered');
-            // console.log('incorrect');
-        }
-    }
-    /**
-     *  Picker wheel number select and close functionality
-     */
-    pickerWheelFunctionality() {
-        // Selecting a number from the wheel
-        $('.num-btn').on('click', function (e) {
-            // console.log(e.target.value);
-            const id = $('#picker-wheel').attr('data-tilenumber');
-            // console.log('found id: ', id);
-            $(`#${id}`).html(e.target.value);
-            const correct = $(`#${id}`).data('correctanswer')
-            const guess = e.target.value;
-            // console.log('correct: --', correct);
-            // console.log('guess: ---', guess);
-            if (correct == guess) {
-                $(`#${id}`).data('correct', 1);
-                $(`#${id}`).addClass('correct');
-                // console.log('correct');
-                const total = $('.correct');
-                // console.log('total correct: ', total.length)
-
-                // Win here
-                if (total.length > 80) {
-                    alert('Winner!');
-
-                    // disable all
-                    $('.tile-number').attr('data-changeable', 'no');
-                    // console.log($('.tile-number'))
-                }
-            } else {
-                $(`#${id}`).data('correct', 0);
-                $(`#${id}`).removeClass('correct');
-                // console.log('incorrect');
-            }
-
-
-            $('#picker-wheel').hide();
-        
-            //update game
-        });
-
-        // Close wheel button
-        $('#close-wheel-btn').on('click', function () {
-            $('#picker-wheel').attr('data-tilenumber', "");
-            $('#picker-wheel').hide();
-        })
-    }
-
-
-    /**
-     * Picks to random numbers and swaps them in the loadout
-     */
-    swapLoadoutValues(numberOfSwaps) {
+    swapLoadoutValues({numberOfSwaps}) {
         const swapValues = (selected, first, second) => {
             if (selected === first) return second;
             if (selected === second) return first;
             return selected;
-        }
+        };
 
-        // Do 3 times
-        for (let i=0; i<numberOfSwaps; i++) {
+        for (let i = 0; i < numberOfSwaps; i++) {
             const value1 = (Math.floor(Math.random() * 9)) + 1;
             const value2 = (Math.floor(Math.random() * 9)) + 1;
             // swap values
             this.loadout = this.loadout.map(row => {
-                row = row.map(seleced => {
-                    return swapValues(seleced, value1, value2);
-                });
+                row = row.map(seleced => swapValues(seleced, value1, value2));
                 return row;
             });
         }
     }
 
-    shuffleSections() {
+    /**
+     *  Shuffles rows around to make the puzzle appear to be a brand new loadout.
+     *  Sort of like turning a row on a rubik's cube
+     */
+    shuffleLoadout() {
         const selected = (Math.floor(Math.random() * 3));
         const shuffleOptions = {
             0: () => {
-                // console.log('first');
                 this.loadout = [
                     this.loadout[3],
                     this.loadout[4],
@@ -214,10 +68,9 @@
                     this.loadout[0],
                     this.loadout[1],
                     this.loadout[2],
-                ]
+                ];
             },
             1: () => {
-                // console.log('second');
                 this.loadout = [
                     this.loadout[6],
                     this.loadout[7],
@@ -228,10 +81,9 @@
                     this.loadout[3],
                     this.loadout[4],
                     this.loadout[5],
-                ]
+                ];
             },
             2: () => {
-                // console.log('third');
                 this.loadout = [
                     this.loadout[0],
                     this.loadout[1],
@@ -242,42 +94,47 @@
                     this.loadout[3],
                     this.loadout[4],
                     this.loadout[5],
-                ]
+                ];
 
             },
-        }
+        };
 
         // Perform the shuffle
         shuffleOptions[selected]();
     }
 
+
+    /**
+     *  Convert loadout into a 2d array. Makes things easier and cleaner when using .map
+     */
     flattenLoadout() {
         const flatLoadout = [];
-        for (let row=0; row<9; row++) {
-            for (let col=0; col<9; col++) {
+        for (let row = 0; row < 9; row++) {
+            for (let col = 0; col < 9; col++) {
                 flatLoadout.push(this.loadout[row][col]);
             }
         }
         this.loadout = flatLoadout;
     }
 
-
-    fillBoard() {
+    /**
+     *  Return the html for a new game. Gets placed into the puzzle container
+     */
+    createBoard() {
         const puzzleRow = (rowNumber) => {
             let puzzleRow = '';
-            for (let i=0; i<9; i++) {
+            for (let i = 0; i < 9; i++) {
                 const id = (rowNumber * 9) + i;
                 const covered = (Math.floor(Math.random() * 100) > this.difficulty);
-                // const covered = 0;
                 const p = {
                     id,
                     correctanswer: this.loadout[id],
                     changeable: (covered) ? 'yes' : 'no',
                     correct: (covered) ? '' : 'correct',
                     covered: (covered) ? 'covered' : '',
-                    html: (!covered) ?  this.loadout[id] : '', 
-                }
-                puzzleRow = puzzleRow + (`
+                    html: (!covered) ? this.loadout[id] : '',
+                };
+                puzzleRow += (`
                         <div class="puzzle-tile d-flex">
                             <p id="${p.id}"
                                 class="m-auto tile-number ${p.covered} ${p.correct}"
@@ -288,11 +145,11 @@
                     `);
             }
             return puzzleRow;
-        }
+        };
         const board = () => {
             let board = '';
-            for (let i=0; i<9; i++) {
-                board = board + (`
+            for (let i = 0; i < 9; i++) {
+                board += (`
                         <div class="row d-flex justify-content-center puzzle-row">
                             ${puzzleRow(i)}
                         </div>
@@ -300,14 +157,10 @@
                 );
             }
             return board;
-        }
+        };
 
         const html = board();
-        $('#puzzle-container').html('');
-        $('#puzzle-container').append(html);
-
+        return html;
     }
-
-
 }
 
--- docs/js/sudoku.js
+++ docs/js/sudoku.js
@@ -1,11 +1,115 @@
 
-let game = new Game('easy');
 
-game.initialize();
-
-$('#new-game').on('click', function () {
-    const difficulty = $('input[name=difficulty]:checked').val();
-    game = new Game(difficulty);
-    game.initialize();
-    console.log(`Loaded ${difficulty}`);
-});
+/**
+ *  Page load setup
+ */
+ let game = new Game('easy');
+ $('#puzzle-container').html('');
+ $('#puzzle-container').append(game.initialize());
+ pickerWheelFunctionality();
+ tileClickFunctionality();
+ 
+ 
+ /**
+  *  New game clicked
+  */
+ $('#new-game').on('click', function () {
+     const difficulty = $('input[name=difficulty]:checked').val();
+     game = new Game(difficulty);
+     $('#puzzle-container').html('');
+     $('#puzzle-container').append(game.initialize());
+     tileClickFunctionality();
+     console.log(`Loaded ${difficulty}`);
+ });
+ 
+ 
+ 
+ /**
+  *  Setup picker wheel
+  */
+ function pickerWheelFunctionality () {
+     // Selecting a number from the wheel
+     $('.num-btn').on('click', function (e) {
+         const id = $('#picker-wheel').attr('data-tilenumber');
+         $(`#${id}`).html(e.target.value);
+         const correct = $(`#${id}`).data('correctanswer')
+         const guess = e.target.value;
+         if (correct == guess) {
+             $(`#${id}`).data('correct', 1);
+             $(`#${id}`).addClass('correct');
+             const total = $('.correct');
+ 
+             // Win here
+             if (total.length > 80) {
+                 alert('Winner!');
+ 
+                 // disable all
+                 $('.tile-number').attr('data-changeable', 'no');
+             }
+         } else {
+             $(`#${id}`).data('correct', 0);
+             $(`#${id}`).removeClass('correct');
+         }
+ 
+         $('#picker-wheel').hide();
+         // update game
+     });
+ 
+     // Close wheel button
+     $('#close-wheel-btn').on('click', function () {
+         $('#picker-wheel').attr('data-tilenumber', '');
+         $('#picker-wheel').hide();
+     });
+ }
+ 
+ 
+ 
+ /**
+  *  Setup tile clicks
+  */
+ function tileClickFunctionality() {
+     // Tile on click, spawns a picker wheel
+     $('.puzzle-tile').on('click', function (e) {
+         const x = e.pageX;
+         const y = e.pageY;
+         const currentTile = e.target.firstElementChild;
+         const changeable = e.target.firstElementChild.getAttribute('data-changeable');
+         // console.log('changeable', changeable);
+         if (changeable === 'yes') {
+             // console.log('tripped');
+             spawnPickerWheel({ x, y, currentTile });
+         }
+ 
+     });
+ 
+     // Show wheel
+     const spawnPickerWheel = ({ x, y, currentTile }) => {
+         // const correctanswer = currentTile.attributes[2].value || 'nope';
+         const id = currentTile.id;
+         // const isHidden = e.target.firstElementChild.getAttribute('data-isHidden');
+         // console.log('correct answer: ' + correctanswer);
+         // console.log('id??: ', id);
+         $('#picker-wheel').attr('data-tilenumber', id);
+         // console.log(`x: ${x}, y: ${y}`);
+         x = x - 130;
+         y = y - 130;
+         // console.log('id: ' + tileNumber);
+         if (screen.width > 700) {
+             $('#picker-wheel').css({
+                 "top": y,
+                 "left": x,
+             });
+         } else {
+             const xOffset = (screen.width / 2 - 125);
+             const yOffset = (screen.height / 2 - 125);
+             $('#picker-wheel').css({
+                 "bottom": `${yOffset}px`,
+                 "left": `${xOffset}px`,
+             });
+         }
+ 
+         $('#picker-wheel').removeClass('d-none').show(800);
+ 
+         // Check if tile correct or not
+     };
+ }
