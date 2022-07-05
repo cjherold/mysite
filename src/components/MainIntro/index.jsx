286c940b93054864d58ddd5846c99e15d813c75f
Chris <64932887+cjherold@users.noreply.github.com>
2022-07-05 17:59:20 -0500

    getting things running with react

--- src/components/MainIntro/index.jsx
+++ src/components/MainIntro/index.jsx
@@ -1,25 +1,28 @@
 // import './style.scss';
-import { Container, Row } from "react-bootstrap";
-
+import { Container, Row } from 'react-bootstrap';
 
 function MainIntro() {
 	return (
-        <>
-            <Container id="comp-mainintro" fluid>
-                <Row>
-                    <div className="col-md-5 col-12 d-flex">
-                        <div className="m-auto col-12">
-                            <h1 className="m-auto coloring-book col-12">Christopher Herold</h1>
-                            <p className='coloring-book-subtext col-12'>Web Developer</p>
-                        </div>
-                    </div>
+		<>
+			<Container id='comp-mainintro' fluid>
+				<Row>
+					<div className='col-md-5 col-12 d-flex'>
+						<div className='m-auto col-12'>
+							<h1 className='m-auto coloring-book col-12'>Christopher Herold</h1>
+							<p className='coloring-book-subtext col-12'>Web Developer</p>
+						</div>
+					</div>
 
-                    <div className="col-md-7 col-12 d-flex">
-                        <img className="img-fluid shadow" src="https://via.placeholder.com/800x500" alt="main" />
-                    </div>
-                </Row>
-            </Container>
-        </>
+					<div className='col-md-7 col-12 d-flex'>
+						<img
+							className='img-fluid shadow'
+							src='./assets/img/daisy-outside.png'
+							alt='main'
+						/>
+					</div>
+				</Row>
+			</Container>
+		</>
 	);
 }
 
