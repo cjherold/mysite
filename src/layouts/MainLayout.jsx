1236f7853624d87e984c2dba398951b45924f778
Chris <64932887+cjherold@users.noreply.github.com>
2022-08-10 20:35:59 -0500

    simplified dark mode check

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,22 +1,16 @@
 import { TopNav, Footer } from '../components';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import $ from 'jquery';
 
 
 
 export default function Main({ pageName, children }) {
-    const [isDark, setIsDark] = useState(false);
-
 	// Set dark/light mode according to the browswer
 	useEffect(() => {
 		// const usingDarkMode = !(window?.matchMedia('(prefers-color-scheme: dark)').matches);
 		const usingDarkMode = false;
-		setIsDark(usingDarkMode);
-		if (isDark) {
-			$('html').addClass('dark-theme');
-		}
-	}, [isDark]);
-
+		if (usingDarkMode) return $('html').addClass('dark-theme');
+	}, []);
 
     // Wrapper for each page that adds nav and footer
     return (
